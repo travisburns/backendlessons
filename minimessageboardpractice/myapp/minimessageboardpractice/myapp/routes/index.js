fd1566eb859ce4465ca9589b0fadd1c5d0fa0dcf
travisburns <41456635+travisburns@users.noreply.github.com>
2022-06-03 23:01:04 -0700

    Changed style and fixed missing messages error

---
+++ minimessageboardpractice/myapp/minimessageboardpractice/myapp/routes/index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+
+const messages = [
+  {
+    text: "hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+{
+  text: "hello World!",
+  user: "Charles",
+  added: new Date()
+},
+
+
+];
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
+});
+
+/* GET new form */
+router.get('/new', function (req, res, next) {
+  res.render('form', {title: 'Mini Messageboard', messages: messages });
+});
+
+
+/* Post new submission */
+router.post('/new', function (req, res, next) {
+  const { name, message } = req.body;
+  messages.push({ user: name, text: message, date: new Date()  });
+  res.redirect('/');
+});
+
+module.exports = router;
