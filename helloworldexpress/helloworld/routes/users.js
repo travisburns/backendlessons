51c68aac8068e8994db3f9eb757654ea2eb57dad
travisburns <41456635+travisburns@users.noreply.github.com>
2022-06-08 14:22:34 -0700

    Express Routes Practice
    Practicing making routes for expressjs

---
+++ helloworldexpress/helloworld/routes/users.js
@@ -0,0 +1,17 @@
+var express = require('express');
+const { append } = require('express/lib/response');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/cool', function(req, res, next) {
+  res.send("you're so cool");
+});
+
+
+
+module.exports = router;
+
