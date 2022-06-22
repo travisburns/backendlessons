a02e1adf567e2c63f89b6df022bf949269b08e38
travisburns <41456635+travisburns@users.noreply.github.com>
2022-06-21 17:12:38 -0700

    Add User Application
    An application that allows for the addition, modification, and deletion of users. Logs user data and images from a form. Used with NodeJs, Express, Ejs, and stored in MongoDB.

--- useraddapp/routes/routes.js
+++ useraddapp/routes/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/users');
 const multer = require('multer');
-
+const fs = require('fs');
 // image upload
 
 var storage = multer.diskStorage({
@@ -61,5 +61,88 @@ router.get('/', (req, res) => {
 router.get('/add', (req, res) => {
     res.render('add_users', { title: 'Add Users'});
 });
+
+//edit user route
+router.get('/edit/:id', (req, res) => {
+    let id = req.params.id;
+    User.findById(id, (err, user)=>{
+        if (err) {
+            res.redirect('/');
+        } else {
+            if (user == null){
+                res.redirect('/');
+            } else {
+                res.render('edit_users', {
+                    title: 'Edit User',
+                    user: user,
+                });
+            }
+        }
+    });
+});
+
+
+//Update user route
+router.post('/update/:id', upload, (req, res) => {
+    let id = req.params.id;
+    let new_image = '';
+
+    if(req.file){
+        new_image = req.file.filename;
+        try{
+            fs.unlinkSync('./uploads/' + req.body.old_image);
+        } catch(err) {
+            console.log(err);
+        }
+    } else {
+        new_image = req.body.old_image;
+    }
+
+User.findByIdAndUpdate(id, {
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    image: new_image,
+}, (err, result) => {
+    if(err){
+        res.json({ message: err.message, type: 'danger' });
+    } else {
+        req.session.message = {
+            type: 'success',
+            message: 'User updated successfully',
+        };
+        res.redirect('/');
+    }
+})
+
+});
+
+
+// Delete user route
+
+router.get('/delete/:id', (req, res) => {
+    let id = req.params.id;
+    User.findByIdAndRemove(id, (err, result) => {
+        if(result.image != '') {
+            try{
+                fs.unlinkSync('./uploads/'+ result.image);
+            } catch(err) {
+                console.log(err);
+            }
+        }
+
+        if(err){
+            res.json({ message: err.message });
+        } else {
+            req.session.message = {
+                type: 'info',
+                message: 'User deleted successfully!'
+            };
+            res.redirect("/");
+        }
+
+    })
+})
+
 
 module.exports = router;
